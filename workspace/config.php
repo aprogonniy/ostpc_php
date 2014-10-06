1ac6cf4fec8e1a47dc187ad01cc78d5481dbf597
Stas <stas249501@gmail.com>
2014-10-07 00:48:48 +0300

    Add project

---
+++ workspace/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\development-FTU\workspace/catalog/');
+define('DIR_SYSTEM', 'C:\development-FTU\workspace/system/');
+define('DIR_DATABASE', 'C:\development-FTU\workspace/system/database/');
+define('DIR_LANGUAGE', 'C:\development-FTU\workspace/catalog/language/');
+define('DIR_TEMPLATE', 'C:\development-FTU\workspace/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\development-FTU\workspace/system/config/');
+define('DIR_IMAGE', 'C:\development-FTU\workspace/image/');
+define('DIR_CACHE', 'C:\development-FTU\workspace/system/cache/');
+define('DIR_DOWNLOAD', 'C:\development-FTU\workspace/download/');
+define('DIR_LOGS', 'C:\development-FTU\workspace/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
