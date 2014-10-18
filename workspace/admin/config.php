c680de5dd1ad3838c7c08bab7733924570f0866f
Stas <stas249501@gmail.com>
2014-10-18 19:20:47 +0300

    add module: Also Purchased

---
+++ workspace/admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+define('HTTP_IMAGE', 'http://localhost/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/admin/');
+define('HTTPS_CATALOG', 'http://localhost/');
+define('HTTPS_IMAGE', 'http://localhost/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\development-FTU\workspace/admin/');
+define('DIR_SYSTEM', 'C:\development-FTU\workspace/system/');
+define('DIR_DATABASE', 'C:\development-FTU\workspace/system/database/');
+define('DIR_LANGUAGE', 'C:\development-FTU\workspace/admin/language/');
+define('DIR_TEMPLATE', 'C:\development-FTU\workspace/admin/view/template/');
+define('DIR_CONFIG', 'C:\development-FTU\workspace/system/config/');
+define('DIR_IMAGE', 'C:\development-FTU\workspace/image/');
+define('DIR_CACHE', 'C:\development-FTU\workspace/system/cache/');
+define('DIR_DOWNLOAD', 'C:\development-FTU\workspace/download/');
+define('DIR_LOGS', 'C:\development-FTU\workspace/system/logs/');
+define('DIR_CATALOG', 'C:\development-FTU\workspace/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
---
+++ workspace/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+define('HTTP_IMAGE', 'http://localhost/image/');
+define('HTTP_ADMIN', 'http://localhost/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+define('HTTPS_IMAGE', 'http://localhost/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\development-FTU\workspace/catalog/');
+define('DIR_SYSTEM', 'C:\development-FTU\workspace/system/');
+define('DIR_DATABASE', 'C:\development-FTU\workspace/system/database/');
+define('DIR_LANGUAGE', 'C:\development-FTU\workspace/catalog/language/');
+define('DIR_TEMPLATE', 'C:\development-FTU\workspace/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\development-FTU\workspace/system/config/');
+define('DIR_IMAGE', 'C:\development-FTU\workspace/image/');
+define('DIR_CACHE', 'C:\development-FTU\workspace/system/cache/');
+define('DIR_DOWNLOAD', 'C:\development-FTU\workspace/download/');
+define('DIR_LOGS', 'C:\development-FTU\workspace/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
