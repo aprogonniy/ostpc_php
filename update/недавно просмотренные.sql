ecc9e9f00b0a2e6b6dccc6b0779a6c4284ccd63a
Anton <Anton@192.168.100.114>
2014-11-08 10:48:17 +0200

    Добавлен модуль недавно просмотренные

---
+++ update/недавно просмотренные.sql
@@ -0,0 +1,3 @@
+INSERT INTO `oc_extension` ( `type`, `code`) VALUES ( 'module', 'product_viewed');
+ INSERT INTO `oc_setting` ( `store_id`, `group`, `key`, `value`, `serialized`) VALUES ( 0, 'product_viewed', 'product_viewed_module', 'a:1:{i:0;a:7:{s:5:"limit";s:1:"5";s:11:"image_width";s:2:"80";s:12:"image_height";s:2:"80";s:9:"layout_id";s:1:"6";s:8:"position";s:11:"column_left";s:6:"status";s:1:"1";s:10:"sort_order";s:0:"";}}', 1);# Затронута 1 строка. # Затронута 1 строка. # MySQL вернула пустой результат (т.е. ноль строк).
+																																																																													
