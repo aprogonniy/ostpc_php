907007f2e4e1b74dcebe279c94c11095ef314127
Женя <didevgen@gmail.com>
2014-11-12 10:11:41 +0200

    обновление БД

---
+++ _sources/Watermark.sql
@@ -0,0 +1,4 @@
+INSERT INTO `oc_extension` (`extension_id`, `type`, `code`) VALUES
+(445, 'module', 'watermark');
+INSERT INTO `oc_setting` (`setting_id`, `store_id`, `group`, `key`, `value`, `serialized`) VALUES
+(961, 0, 'watermark', 'watermark', 'a:14:{s:6:"status";s:1:"1";s:4:"type";s:3:"img";s:5:"image";s:13:"data/logo.png";s:14:"watermark_text";s:0:"";s:14:"watermark_font";s:9:"arial.ttf";s:19:"watermark_font_size";s:2:"12";s:5:"color";s:5:"0,0,0";s:5:"angle";s:1:"0";s:8:"position";s:8:"topright";s:6:"offset";a:2:{s:1:"x";s:1:"0";s:1:"y";s:1:"0";}s:7:"scaling";a:2:{s:5:"width";s:3:"250";s:6:"height";s:3:"250";}s:7:"opacity";s:1:"7";s:3:"min";a:2:{s:5:"width";s:0:"";s:6:"height";s:0:"";}s:7:"folders";a:1:{i:0;s:9:"data/demo";}}', 1);
