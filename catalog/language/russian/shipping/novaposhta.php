f84b5c9d36fa6561bef350c016f79ad9d0e47c88
kostemkin <kostemkin@192.168.1.6>
2014-11-21 21:18:04 +0200

    AddLinkNovaPochta

--- catalog/language/russian/shipping/novaposhta.php
+++ catalog/language/russian/shipping/novaposhta.php
@@ -1,5 +1,5 @@
 <?php
 // Text
 $_['text_title']       = 'Доставка Новой Почтой';
-$_['text_description'] = 'Наши сотрудники отправят товар Новой Почтой.';
+$_['text_description'] = '<a href="http://novaposhta.ua/office" target="_blank">Список складов по городам</a> ';
 ?>
